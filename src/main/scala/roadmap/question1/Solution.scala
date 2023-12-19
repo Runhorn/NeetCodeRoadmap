b97ea71fb2d6843552eddf108595577f74e9dd6b
Jakub <jakub.kurcwald@gmail.com>
2023-12-19 21:32:13 +0100

    first commit

---
+++ src/main/scala/roadmap/question1/Solution.scala
@@ -0,0 +1,13 @@
+package roadmap.question1
+
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ */
+object Solution extends App {
+  def containsDuplicate(nums: Array[Int]): Boolean = {
+    nums.toSet.size < nums.length
+  }
+
+  val numbers = Array(1,1,1,3,3,4,3,2,4,2)
+  println(containsDuplicate(numbers))
+}
