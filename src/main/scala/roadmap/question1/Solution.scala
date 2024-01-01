8f25fc76ceca90fed050aea1fecf81a8391ce7fb
Jakub <jakub.kurcwald@gmail.com>
2024-01-01 15:17:34 +0100

    Applied scala fmt

--- src/main/scala/roadmap/question1/Solution.scala
+++ src/main/scala/roadmap/question1/Solution.scala
@@ -1,9 +1,9 @@
 package roadmap.question1
 
-/**
- * https://leetcode.com/problems/contains-duplicate/
- * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
- */
+/** https://leetcode.com/problems/contains-duplicate/ Given an integer array
+  * nums, return true if any value appears at least twice in the array, and
+  * return false if every element is distinct.
+  */
 object Solution extends App {
   def containsDuplicate(nums: Array[Int]): Boolean = {
     nums.toSet.size < nums.length
--- src/main/scala/roadmap/question3/Solution.scala
+++ src/main/scala/roadmap/question3/Solution.scala
@@ -1,15 +1,16 @@
 package roadmap.question3
 
-/**
- * https://leetcode.com/problems/two-sum/
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- */
+/** https://leetcode.com/problems/two-sum/ Given an array of integers nums and
+  * an integer target, return indices of the two numbers such that they add up
+  * to target.
+  */
 object Solution extends App {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val numIndices = scala.collection.mutable.HashMap[Int, Int]()
     for ((num, index) <- nums.zipWithIndex) {
       val complement = target - num
-      if (numIndices.contains(complement)) return Array(numIndices(complement), index)
+      if (numIndices.contains(complement))
+        return Array(numIndices(complement), index)
       else numIndices(num) = index
     }
     throw new Exception("Solution not found")
